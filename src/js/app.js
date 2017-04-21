2d95722336d1c7982ba9b4e138a26c7350bd860f
Zhang Zengbo <zengbo.zhang@gmail.com>
2017-04-21 14:28:09 +0800

    small style adjustment

--- src/js/app.js
+++ src/js/app.js
@@ -6,5 +6,5 @@ window.calculate = function(){
     var pkey = bitsharesjs.PrivateKey.fromWif(document.getElementById('wif').value);
     var message = document.getElementById('message').value;
     var result = bitsharesjs.Signature.signBuffer(new Buffer(message, 'utf8'), pkey);
-    document.getElementById('result').value = result.toHex();
+    document.getElementById('result').innerHTML = result.toHex();
 }
